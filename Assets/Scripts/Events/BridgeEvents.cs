d865fd6ebb66d4602bd58eed70291559300c87d7
houyja <houyja@gmail.com>
2017-01-20 23:39:38 -0500

    Initial Commit

---
+++ Assets/Scripts/Events/BridgeEvents.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeEvents : MonoBehaviour {
+    public GameObject bridge;
+
+    public float BridgeOpenDelay;
+    public float BridgeTimeLimit;
+
+    bool LeftTrigger = false;
+    private float OpenTime;
+    private float ClosingTime;
+
+    private string BridgeStatus;
+
+    private const float UpdateTime = (float).02;
+
+    // Use this for initialization
+    void Start () {
+        BridgeStatus = "Closed";
+        OpenTime = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        switch(BridgeStatus)
+        {
+            case "Opening":
+                OpenTime += UpdateTime;
+                if(OpenTime >= BridgeOpenDelay)
+                {
+                    BridgeStatus = "Open";
+                    ActivateBridge(true);
+                    OpenTime = 0;
+                    
+                }
+                break;
+            case "Open":
+                if(LeftTrigger)
+                    ClosingTime += UpdateTime;
+
+                if(ClosingTime > BridgeTimeLimit)
+                {
+                    BridgeStatus = "Closed";
+                    ActivateBridge(false);
+                    ClosingTime = 0;
+                }
+                break;
+            case "Closed":
+                break;
+        }
+    }
+
+    private void ActivateBridge(bool active)
+    {
+        bridge.SetActive(active);
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        BridgeStatus = "Opening";
+        LeftTrigger = false;
+    }
+
+    public void OnCollisionExit(Collision collision)
+    {
+        LeftTrigger = true;
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+    public float moveSpeed;
+    public float turnSpeed;
+    public float resetRotationTimeLimit;
+    public float WorldBoundary_Bottom;
+
+    private float UnalignedTime = 0;
+    private Vector3 RestorePoint = new Vector3(0,0,0);
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerMovement();
+        CheckBoundaryViolation();
+        CheckAlignment();
+    }
+
+    private void CheckAlignment()
+    {
+        if (System.Math.Abs(transform.rotation.eulerAngles.x) > 45 || (System.Math.Abs(transform.rotation.eulerAngles.z) > 45))
+        {
+            UnalignedTime += (float).02;
+
+            if (UnalignedTime > resetRotationTimeLimit)
+            {
+                ResetAlignment();
+                UnalignedTime = 0;
+            }
+        }
+        else
+        {
+            UnalignedTime = 0;
+        }
+    }
+    private void PlayerMovement()
+    {
+        float MoveForward = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        float MoveRotate = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
+        transform.Translate(Vector3.forward * MoveForward);
+        transform.Rotate(0, MoveRotate, 0);
+    }
+    private void CheckBoundaryViolation()
+    {
+        if (transform.position.y < WorldBoundary_Bottom)
+        {
+            ResetPosition();
+            ResetAlignment();
+        }
+    }
+
+
+    private void ResetAlignment()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+    private void ResetPosition()
+    {
+        transform.position = RestorePoint;
+    }
+}
